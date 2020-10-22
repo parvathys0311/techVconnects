38974b25aa8033cf2fa2d57bcf22e587eef95303
Parvathy <parvathys0311@gmail.com>
2020-10-22 08:48:03 -0600

    css-tweaks

--- js/events.js
+++ js/events.js
@@ -2,7 +2,5 @@ let selected_event = document.querySelector('.event-detail')
 let a_link = document.querySelector('.link')
 
 a_link.addEventListener('click', function toggle() {
-
   selected_event.classList.toggle('hidden');
-
 });
